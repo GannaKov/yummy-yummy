da0ca680f9ea3545102dd70d106de3140b903a5b
GannaKov <zlatta2000@gmail.com>
2024-03-02 14:54:13 +0100

    details

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -5,6 +5,7 @@ import { testRest } from "../../fakeData";
 import { getSingleRestaurantById } from "../../services/requests";
 import styles from "./DetailsPage.module.css";
 import {
+  Box,
   Card,
   CardBody,
   CardFooter,
@@ -34,85 +35,105 @@ const DetailsPage = () => {
       });
   }, [id]);
   return (
-    <div>
+    <div className={styles.pageWrp}>
+      {/* <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">  </Box> */}
+
       {isLoading && <SpinnerOur />}
       {restaurant && (
         <>
-          {" "}
-          <h1>{restaurant.name}</h1>
-          <img src={restaurant.img_url} alt={restaurant.name} />
-          <p>{restaurant.city_name}</p>
-          <p>{restaurant.address}</p>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <div className={styles.timeTableWrp}>
-              <p>Monday</p>
-              <div>
-                <p>{restaurant.timetable.Monday.open}</p>
-                <p>{restaurant.timetable.Monday.close}</p>
+          <h1 className={styles.restTitle}>{restaurant.name}</h1>
+          <img
+            src={restaurant.img_url}
+            alt={restaurant.name}
+            className={styles.restImg}
+          />
+          <div>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+            >
+              <div className={styles.timeTableWrp}>
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Monday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Monday.open}</p>
+                    <p>-</p>
+                    <p>{restaurant.timetable.Monday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Tuesday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Tuesday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Tuesday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Wednesday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Wednesday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Wednesday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  {" "}
+                  <p className={styles.restTimeDay}>Thursday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Thursday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Thursday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Friday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Friday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Friday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Saturday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Saturday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Saturday.close}</p>
+                  </div>
+                </div>
+
+                <div className={styles.restTimeInnerWrp}>
+                  <p className={styles.restTimeDay}>Sunday</p>
+                  <div className={styles.restTimeWrp}>
+                    <p>{restaurant.timetable.Sunday.open}</p> <p>-</p>
+                    <p>{restaurant.timetable.Sunday.close}</p>
+                  </div>
+                </div>
               </div>
 
-              <p>Tuesday</p>
-              <div>
-                <p>{restaurant.timetable.Tuesday.open}</p>
-                <p>{restaurant.timetable.Tuesday.close}</p>
-              </div>
+              <Stack>
+                <CardBody>
+                  <Heading size="sm">{restaurant.name}</Heading>
 
-              <p>Wednesday</p>
-              <div>
-                <p>{restaurant.timetable.Wednesday.open}</p>
-                <p>{restaurant.timetable.Wednesday.close}</p>
-              </div>
+                  <Text py="2">
+                    Caffè latte is a coffee beverage of Italian origin made with
+                    espresso and steamed milk.
+                  </Text>
+                </CardBody>
 
-              <p>Thursday</p>
-              <div>
-                {" "}
-                <p>{restaurant.timetable.Thursday.open}</p>
-                <p>{restaurant.timetable.Thursday.close}</p>
-              </div>
-
-              <p>Friday</p>
-              <div>
-                <p>{restaurant.timetable.Friday.open}</p>
-                <p>{restaurant.timetable.Friday.close}</p>
-              </div>
-
-              <p>Saturday</p>
-              <div>
-                <p>{restaurant.timetable.Saturday.open}</p>
-                <p>{restaurant.timetable.Saturday.close}</p>
-              </div>
-
-              <p>Sunday</p>
-              <div>
-                <p>{restaurant.timetable.Sunday.open}</p>
-                <p>{restaurant.timetable.Sunday.close}</p>
-              </div>
-            </div>
-
-            <Stack>
-              <CardBody>
-                <Heading size="sm">{restaurant.name}</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Text variant="solid" colorScheme="blue">
-                  {restaurant.city_name}
-                </Text>
-                <Text variant="solid" colorScheme="blue">
-                  {restaurant.address}
-                </Text>
-              </CardFooter>
-            </Stack>
-          </Card>
+                <CardFooter>
+                  <Text variant="solid" colorScheme="blue">
+                    {restaurant.city_name}
+                  </Text>
+                  <Text variant="solid" colorScheme="blue">
+                    {restaurant.address}
+                  </Text>
+                </CardFooter>
+              </Stack>
+            </Card>
+          </div>
         </>
       )}
     </div>
