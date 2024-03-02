c62cd7f0baa8f59a9dcfdc3689f28fa407b73358
GannaKov <104867435+GannaKov@users.noreply.github.com>
2024-03-02 09:18:22 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Example from "./components/Example/Example";
+
+const App = () => {
+  return (
+    <div>
+      <Example />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/ExamplePage/ExamplePage.jsx
@@ -0,0 +1,16 @@
+import styles from "./ExamplePage.module.css";
+const ExamplePage = () => {
+  return (
+    <main>
+      <h1 className={styles.exampleTitle}>Welcome</h1>
+      {/* <img src="https://via.placeholder.com/960x240" alt="" /> */}
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
+        voluptatum voluptate.
+      </p>
+    </main>
+  );
+};
+
+export default ExamplePage;
