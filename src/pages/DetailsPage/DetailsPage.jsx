3a1620f4d1d4f6802b7115b4bc5e2fe7320e7c24
GannaKov <zlatta2000@gmail.com>
2024-03-02 11:07:44 +0100

    Spinner

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,5 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SpinnerOur from "../../components/Spinner/Spinner";
+import { testRest } from "../../fakeData";
+
 const DetailsPage = () => {
-  return <div>details</div>;
+  const [restaurant, setRestaurant] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   getSingleUserById(id)
+  //     .then((res) => {
+  //       setUser(res);
+  //     })
+  //     .catch((error) => console.log(error.message))
+  //     .finally(() => {
+  //       setLoading(false);
+  //       setIsFetch(true);
+  //     });
+  // }, [id]);
+  return (
+    <div>
+      <SpinnerOur />
+    </div>
+  );
 };
 
 export default DetailsPage;
